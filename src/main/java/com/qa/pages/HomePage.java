6e0b0f90d826a2645981892546efb06cde09e179
Vikramjeet Singh Sangha <vsangha@tibco.com>
2022-12-27 13:24:16 +0530

    Adding methods and elements in homepage class

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -1,5 +1,34 @@
 package com.qa.pages;
 
-public class HomePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.qa.base.TestBase;
+
+public class HomePage extends TestBase {
+	
+	@FindBy(id="nav-logo-sprites")
+	WebElement logo;
+	
+	@FindBy(id="nav-hamburger-menu")
+	WebElement hamburgerMenu;
+	
+	public HomePage(){
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Actions:
+	public String validatePageTitle(){
+		return driver.getTitle();
+	}
+		
+	public boolean validatehamburgerMenu(){
+		return hamburgerMenu.isDisplayed();
+	}
+	
+	public boolean validateLogo(){
+		return logo.isDisplayed();
+	}
 
 }
