eb8697e9c73215590d78c848fb37f28af8beb804
Vikramjeet Singh Sangha <vsangha@tibco.com>
2022-12-27 12:13:33 +0530

    Created a maven project skeleton

---
+++ src/main/java/SeleniumDocker/App.java
@@ -0,0 +1,13 @@
+package SeleniumDocker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
