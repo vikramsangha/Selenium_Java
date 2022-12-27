242d890a4dcf34ee8a7cfd63a61c2a4e846064b0
Vikramjeet Singh Sangha <vsangha@tibco.com>
2022-12-27 13:17:06 +0530

    Adding webdriverManager

--- src/main/java/com/qa/base/TestBase.java
+++ src/main/java/com/qa/base/TestBase.java
@@ -4,19 +4,26 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import com.qa.utils.TestUtil;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {
 	
 	public static WebDriver driver;
 	public static Properties prop;
+	public static WebDriverManager webDriverManagerObject = null;
 	
 	public TestBase() {
 		
@@ -37,21 +44,26 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "/Users/naveenkhunteta/Downloads/chromedriver");	
-			driver = new ChromeDriver(); 
+			
+			webDriverManagerObject = WebDriverManager.chromedriver();
+			webDriverManagerObject.cachePath(System.getProperty("user.dir")+ "/src/main/java/com");
+			webDriverManagerObject.setup();
+			
+			HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+	        chromePrefs.put("profile.default_content_settings.popups", 0);
+	        chromePrefs.put("safebrowsing.enabled", "true"); 
+	        ChromeOptions options = new ChromeOptions();
+	        options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+	        options.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR,org.openqa.selenium.UnexpectedAlertBehaviour.ACCEPT);
+	        options.addArguments("disable-popup-blocking");       
+	        options.addArguments("--disable-backgrounding-occluded-windows");
+	        driver = new ChromeDriver(options); 
 		}
-		else if(browserName.equals("FF")){
-			System.setProperty("webdriver.gecko.driver", "/Users/naveenkhunteta/Documents/SeleniumServer/geckodriver");	
-			driver = new FirefoxDriver(); 
+		else {
+			System.out.println("please pass the right browser name......"); 
 		}
 		
-		
-		//e_driver = new EventFiringWebDriver(driver);
-		// Now create object of EventListerHandler to register it with EventFiringWebDriver
-		//eventListener = new WebEventListener();
-		//e_driver.register(eventListener);
-		//driver = e_driver;
-		
+	
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(TestUtil.PAGE_LOAD_TIMEOUT));
